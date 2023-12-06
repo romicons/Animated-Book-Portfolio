7fb30138b336fd91610e63ede8523afe5d2581b7
romicons <romina.constantinoff@gmail.com>
2023-12-05 23:02:05 -0300

    Added Fonts and CSS Pixel Box Library to get a pixel art style in some containers. There is a problem with the animation of the skill-icon with 3.2 rem width or less.

--- index.js
+++ index.js
@@ -11,22 +11,19 @@ const paper5 = document.querySelector("#p5");
 
 const cover = document.querySelector("bottom-cover")
 
-// Event Listener
-prevBtn.addEventListener("click", goPrevPage);
-nextBtn.addEventListener("click", goNextPage);
 
 // Business Logic
 let currentLocation = 1;
 let numOfPapers = 5;
 let maxLocation = numOfPapers + 1;
 
-function openBook() {
+const openBook = () => {
     book.style.transform = "translateX(50%)";
     prevBtn.style.transform = "translateX(-180px)";
     nextBtn.style.transform = "translateX(180px)";
 }
 
-function closeBook(isAtBeginning) {
+const closeBook = (isAtBeginning) => {
     if(isAtBeginning) {
         book.style.transform = "translateX(0%)";
     } else {
@@ -37,7 +34,7 @@ function closeBook(isAtBeginning) {
     nextBtn.style.transform = "translateX(0px)";
 }
 
-function goNextPage() {
+const goNextPage = () => {
     if(currentLocation < maxLocation) {
         switch(currentLocation) {
             case 1:
@@ -64,7 +61,7 @@ function goNextPage() {
     }
 }
 
-function goPrevPage() {
+const goPrevPage = () => {
     if(currentLocation > 1) {
         switch(currentLocation) {
             case 2:
@@ -100,3 +97,66 @@ function goPrevPage() {
         currentLocation--;
     }
 }
+
+// Event Listener
+prevBtn.addEventListener("click", goPrevPage);
+nextBtn.addEventListener("click", goNextPage);
+
+
+// CSS HOUDINI PIXEL BOX LIBRARY FUNCTIONS
+
+if ('paintWorklet' in CSS && 'registerProperty' in CSS && 'CSSUnitValue' in window) {
+        
+    CSS.registerProperty({
+         name: '--pixelbox-border',
+         syntax: '<length>',
+         initialValue: '2px',
+         inherits: false
+     });
+
+     CSS.registerProperty({
+         name: '--pixelbox-border-radius',
+         syntax: '<length>',
+         initialValue: '0px',
+         inherits: false
+     });
+
+     CSS.registerProperty({
+         name: '--pixelbox-border-color',
+         syntax: '<color>',
+         initialValue: '#000000',
+         inherits: false
+     });
+    
+     CSS.registerProperty({
+         name: '--pixelbox-background-color',
+         syntax: '<color>',
+         initialValue: '#ffffff',
+         inherits: false
+     });
+
+     CSS.registerProperty({
+         name: '--pixelbox-background-shadow-border',
+         syntax: '<length>',
+         initialValue: '0px',
+         inherits: false
+     });
+     
+     CSS.registerProperty({
+         name: '--pixelbox-background-shadow-color',
+         syntax: '<color>',
+         initialValue: '#adafbc',
+         inherits: false
+     });
+     
+     /*CSS.registerProperty({
+         name: '--pixelbox-background-shadow-position',
+         syntax: '<string>',
+         initialValue: 'bottom-right',
+         inherits: false
+     });
+     */
+ } else {
+     console.log("Not Supported");
+ } 
+ CSS.paintWorklet.addModule('pixelbox.js');

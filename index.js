c79923de8580f74992c92cc8c1a782f8e354b1e6
romicons <romina.constantinoff@gmail.com>
2023-12-19 18:59:26 -0300

    Added navMarkDown function. Still, a work in progress.

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const paper7 = document.querySelector("#p7");
 
 // FUNCTION DARK/LIGHT MODE
 
-let darkMode =localStorage.getItem('darkMode');
+let darkMode = localStorage.getItem('darkMode');
 const darkModeToggle = document.querySelector('#candle-btn');
 
 const enableDarkMode = () => {
@@ -51,12 +51,16 @@ let currentLocation = 1;
 let numOfPapers = 7;
 let maxLocation = numOfPapers + 1;
 
+    // FUNCTION TO OPEN THE BOOK
+
 const openBook = () => {
     book.style.transform = "translate(50%, 10%)";
     setTimeout(() => {
     prevBtn.style.visibility = "visible";
     }, 500);
 };
+
+    // FUNCTION TO CLOSE THE BOOK
 
 const closeBook = (isAtBeginning) => {
     if(isAtBeginning) {
@@ -66,6 +70,8 @@ const closeBook = (isAtBeginning) => {
         book.style.transform = "translateX(100%)";
     }
 }
+
+    // FUNCTION TO NAVIGATE TO THE NEXT PAGE
 
 const goNextPage = () => {
     if(currentLocation < maxLocation) {
@@ -82,6 +88,7 @@ const goNextPage = () => {
                 paper1.style.zIndex = 1;
                 paper2.classList.add("flipped");
                 paper2.style.zIndex = 2;
+                markerRed();
                 }, 250);
                 break;
             case 3:
@@ -91,8 +98,10 @@ const goNextPage = () => {
                 paper1.style.zIndex = 1;
                 paper2.classList.add("flipped");
                 paper2.style.zIndex = 2;
+                markerRed();
                 paper3.classList.add("flipped");
                 paper3.style.zIndex = 3;
+                markerGreen();
                 }, 250);
                 break;
             case 4:
@@ -102,10 +111,13 @@ const goNextPage = () => {
                 paper1.style.zIndex = 1;
                 paper2.classList.add("flipped");
                 paper2.style.zIndex = 2;
+                markerRed();
                 paper3.classList.add("flipped");
                 paper3.style.zIndex = 3;
+                markerGreen();
                 paper4.classList.add("flipped");
                 paper4.style.zIndex = 4;
+                markerYellow();
                 }, 250);
                 break;  
             case 5:
@@ -115,10 +127,13 @@ const goNextPage = () => {
                 paper1.style.zIndex = 1;
                 paper2.classList.add("flipped");
                 paper2.style.zIndex = 2;
+                markerRed();
                 paper3.classList.add("flipped");
                 paper3.style.zIndex = 3;
+                markerGreen();
                 paper4.classList.add("flipped");
                 paper4.style.zIndex = 4;
+                markerYellow();
                 paper5.classList.add("flipped");
                 paper5.style.zIndex = 5;
                 }, 250);
@@ -130,14 +145,18 @@ const goNextPage = () => {
                 paper1.style.zIndex = 1;
                 paper2.classList.add("flipped");
                 paper2.style.zIndex = 2;
+                markerRed();
                 paper3.classList.add("flipped");
                 paper3.style.zIndex = 3;
+                markerGreen();
                 paper4.classList.add("flipped");
                 paper4.style.zIndex = 4;
+                markerYellow();
                 paper5.style.zIndex = 5;
                 paper5.classList.add("flipped");
                 paper6.style.zIndex = 6;
                 paper6.classList.add("flipped")
+                markerBlue();
                 }, 250);
                 nextBtn.style.visibility = "hidden";
                 break;
@@ -149,6 +168,8 @@ const goNextPage = () => {
     }
 }
 
+    // FUNCTION TO NAVIGATE TO THE PREVIOUS PAGE
+
 const goPrevPage = () => {
     if(currentLocation > 1) {
         switch(currentLocation) {
@@ -159,10 +180,13 @@ const goPrevPage = () => {
                 paper1.style.zIndex = 7;
                 paper2.classList.remove("flipped");
                 paper2.style.zIndex = 6;
+                markerRed();
                 paper3.classList.remove("flipped");
                 paper3.style.zIndex = 5;
+                markerGreen();
                 paper4.classList.remove("flipped");
                 paper4.style.zIndex = 4;
+                markerYellow();
                 paper5.classList.remove("flipped");
                 paper5.style.zIndex = 3;
                 paper6.classList.remove("flipped");
@@ -177,10 +201,13 @@ const goPrevPage = () => {
                 setTimeout(() => {
                 paper2.classList.remove("flipped");
                 paper2.style.zIndex = 6;
+                markerRed();
                 paper3.classList.remove("flipped");
                 paper3.style.zIndex = 5;
+                markerGreen();
                 paper4.classList.remove("flipped");
                 paper4.style.zIndex = 4;
+                markerYellow();
                 paper5.classList.remove("flipped");
                 paper5.style.zIndex = 3;
                 paper6.classList.remove("flipped");
@@ -194,8 +221,10 @@ const goPrevPage = () => {
                 setTimeout(() => {
                 paper3.classList.remove("flipped");
                 paper3.style.zIndex = 5;
+                markerGreen();
                 paper4.classList.remove("flipped");
                 paper4.style.zIndex = 4;
+                markerYellow();
                 paper5.classList.remove("flipped");
                 paper5.style.zIndex = 3;
                 paper6.classList.remove("flipped");
@@ -209,6 +238,7 @@ const goPrevPage = () => {
                 setTimeout(() => {
                 paper4.classList.remove("flipped");
                 paper4.style.zIndex = 4;
+                markerYellow();
                 paper5.classList.remove("flipped");
                 paper5.style.zIndex = 3;
                 paper6.classList.remove("flipped");
@@ -253,6 +283,7 @@ const goPrevPage = () => {
     }
 }
 
+    // FUNCTION TO JUMP ACROSS THE BOOK WITH THE MARKERS
 
 const jumpTo = (page) => {
     if (currentLocation > page) {
@@ -269,6 +300,46 @@ const jumpTo = (page) => {
     }, 250);
 }
 
+
+let navMarker = document.querySelectorAll('.marker');
+let markerElements = Array.from(navMarker);  
+let markdownTip = document.createElement('style');
+
+navMarkDown = (page, markerIndex) => {
+    let markerElement = markerElements[markerIndex];
+    if (currentLocation === page && markerElement) {
+        console.log("entro en funcionamiento");
+
+        markerElement.style.zIndex = "10";
+        
+        setTimeout(() => {
+            markerElement.style.height = "4.6rem";
+            markerElement.style.transition = "height 0.5s";
+            
+            markdownTip.innerHTML = `
+                .marker::before {
+                    position: absolute;
+                    content: '';
+                    width: 2.2rem;
+                    height: 2.2rem;
+                    top: 1rem;
+                    background-color: inherit;
+                    transform: rotate(45deg);
+                    z-index: -1;
+                    animation: goDown 0.5s forwards;
+                }
+            `;
+            document.head.appendChild(markdownTip);
+        }, 500);
+    }
+}
+
+markerRed = () => { navMarkDown(2, 0) };
+markerGreen = () => { navMarkDown(3, 1) };
+markerYellow = () => { navMarkDown(4, 2) };
+markerBlue = () => { navMarkDown(7, 3) };
+
+
 // EVENT LISTENER
 
 prevBtn.addEventListener("click", goPrevPage);
@@ -278,37 +349,12 @@ skillsSection.addEventListener ("click", (event) => jumpTo(3));
 proyectsSection.addEventListener("click", (event) => jumpTo(4));
 contactSection.addEventListener("click", (event) => jumpTo(7));
 
-/*
-const navMarkdown = document.querySelectorAll(".marker");
 
-navMarkdown.forEach(markDown => {
-    markDown.addEventListener("click", () => {
-        let markDownElement = document.getElementById(markDown.id);
-        let markdownTip = document.createElement('style');
-                markDownElement.style.zIndex = "90";
-                setTimeout(() => {
-                    markDownElement.style.height = "4.6rem";
-                    markdownTip.innerHTML = `
-                        .marker::before {
-                            position: absolute;
-                            content: '';
-                            width: 2.2rem;
-                            height: 2.2rem;
-                            top: 3.5rem;
-                            background-color: inherit;
-                            transform: rotate(45deg);
-                            z-index: -1;
-                        }
-                    `;
-                    document.head.appendChild(markdownTip);
-                }, 500); 
-                console.log("funciono") 
-    })
+
+/*navMarkUp.forEach(markUp => {
+    markUp = document.querySelectorAll(".marker");
 });
 */
-
-//const navMarkUp = "";
-
 
 // CSS HOUDINI PIXEL BOX LIBRARY FUNCTIONS
 
